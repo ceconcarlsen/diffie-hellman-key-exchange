89635d821ff1309661d06ac27128a86f9722ccdb
Cecon <cecon.carlsen@unesp.br>
2024-02-23 20:05:57 -0300

    feat: react query

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
+import {
+  QueryClient,
+  QueryClientProvider
+} from '@tanstack/react-query';
+
+// Create a client
+const queryClient = new QueryClient()
+
 import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,7 +24,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      </body>
     </html>
   );
 }
