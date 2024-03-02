9d9bb807c0660096c64b81484686d3ce94798aa1
Cecon <cecon.carlsen@unesp.br>
2024-03-02 01:22:02 -0300

    feat: shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,23 @@
 'use client';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 import { useQuery } from '@tanstack/react-query';
-import { Todo } from './api/todos/route';
+
+import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
+import { useTheme } from 'next-themes';
+
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 
 const useGetTodos = () => {
   return useQuery({
@@ -16,18 +33,52 @@ const useGetTodos = () => {
 };
 
 export default function Home() {
+  const { setTheme } = useTheme();
+
   const { data, isLoading } = useGetTodos();
 
   if (isLoading) return <div>Loading...</div>;
 
   return (
-    <div>
-      <h1>Todos</h1>
-      <ul>
+    <div className="my-4">
+      {/* <h1>Todos</h1> */}
+      {/* <ul>
         {data.map((todo: Todo) => (
           <li key={todo.id}>{todo.title}</li>
         ))}
-      </ul>
+      </ul> */}
+      <div className="my-4">
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Theme" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="light">Light</SelectItem>
+            <SelectItem value="dark">Dark</SelectItem>
+            <SelectItem value="system">System</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline" size="icon">
+            <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <span className="sr-only">Toggle theme</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem onClick={() => setTheme('light')}>
+            Light
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme('dark')}>
+            Dark
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setTheme('system')}>
+            System
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 }
--- providers/index.tsx
+++ providers/index.tsx
@@ -1,18 +1,24 @@
-"use client"
+'use client';
 
-import {
-  QueryClient, QueryClientProvider
-} from '@tanstack/react-query'
-import { PropsWithChildren } from 'react'
+import { ThemeProvider } from '@/components/theme-provider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { PropsWithChildren } from 'react';
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 const Providers = ({ children }: PropsWithChildren) => {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-  )
-}
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+};
 
-export default Providers
-
-
+export default Providers;
